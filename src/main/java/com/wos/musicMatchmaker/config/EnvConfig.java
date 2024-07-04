7dba890eea49e81aeb36db219f6fa9ff3ca30dd7
Byeon.woosung <wsbyeon@naonsoft.com>
2024-07-04 16:48:44 +0900

    Change the way env file path is retrieved
    When importing an env file, change it to be imported as an application.yml environment variable.

--- src/main/java/com/wos/musicMatchmaker/config/EnvConfig.java
+++ src/main/java/com/wos/musicMatchmaker/config/EnvConfig.java
@@ -2,15 +2,19 @@ package com.wos.musicMatchmaker.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EnvConfig {
 
+	@Value("${env.dir}")
+	private String ENV_FILE_DIR;
+
 	@PostConstruct
 	public void loadDotenv() {
 		Dotenv dotenv = Dotenv.configure()
-//								.directory("YOUR .env file dir")
+								.directory(ENV_FILE_DIR)
 								.load();
 
 		dotenv.entries().forEach(entry -> {
